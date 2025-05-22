8fc51256330865dd6b6ccf9fb600c6375289fa07
tami-coder <neoshy11@gmail.com>
2025-05-22 16:22:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("WELCOME TO BIMO SLOT 99")
-st.write("🔥GACORR ANTI RUNGKAD BOSS!🔥") 
+st.write("STOP BERMAIN JUDI!! ") 
 
 st.image("Screenshot_2025-05-21-09-28-23-500_com.google.android.googlequicksearchbox-edit.jpg", width= 200) 
